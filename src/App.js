30a998c2383b6f02c0a0fbb514fd7d9da16f1f94
Rohan Kalsi <rkoalsi2000@gmail.com>
2020-02-12 15:00:49 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import { Layout } from "antd";
 import "./sound.css";
 import Sound from "./Sound";
 
-import audio1 from "./sounds/1.wav";
-import audio2 from "./sounds/2.wav";
-import audio3 from "./sounds/3.wav";
-import audio4 from "./sounds/4.wav";
-import audio5 from "./sounds/5.wav";
-import audio6 from "./sounds/6.wav";
-import audio7 from "./sounds/7.mp3";
-import audio8 from "./sounds/8.wav";
-import audio9 from "./sounds/9.wav";
-import audio10 from "./sounds/10.wav";
-import audio11 from "./sounds/11.mp3";
-import audio12 from "./sounds/12.wav";
-import audio13 from "./sounds/13.wav";
-import audio14 from "./sounds/14.wav";
-import audio15 from "./sounds/15.aiff";
-import audio16 from "./sounds/16.wav";
+import audio1 from "./tracks/1.wav";
+import audio2 from "./tracks/2.wav";
+import audio3 from "./tracks/3.wav";
+import audio4 from "./tracks/4.wav";
+import audio5 from "./tracks/5.wav";
+import audio6 from "./tracks/6.wav";
+import audio7 from "./tracks/7.mp3";
+import audio8 from "./tracks/8.wav";
+import audio9 from "./tracks/9.wav";
+import audio10 from "./tracks/10.wav";
+import audio11 from "./tracks/11.mp3";
+import audio12 from "./tracks/12.wav";
+import audio13 from "./tracks/13.wav";
+import audio14 from "./tracks/14.wav";
+import audio15 from "./tracks/15.aiff";
+import audio16 from "./tracks/16.wav";
 const { Content } = Layout;
 
 function App() {
